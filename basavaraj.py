6341a301b7cfe7ed475d1ce48bc3ac130aa28606
sbasavaraja145-netizen <sbasavaraja145@gmail.com>
2026-09-25 20:00:19 +0530

    Add number guessing game in basavaraj.py

---
+++ basavaraj.py
@@ -0,0 +1,20 @@
+import random
+
+def play_game():
+    number = random.randint(1, 100)
+    attempts = 0
+    print("I'm thinking of a number between 1 and 100!")
+
+    while True:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        if guess < number:
+            print("Too low! Try again.")
+        elif guess > number:
+            print("Too high! Try again.")
+        else:
+            print(f"Correct! You guessed it in {attempts} attempts.")
+            break
+
+play_game()
